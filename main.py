0a6c525171aed9232aa1183f3a8a771533aca4a5
somakai_sumasi <ueki@novasto.co.jp>
2024-03-19 09:52:46 +0900

    ファイル行数を読む仕組みに変更

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import re
 import time
@@ -8,64 +9,62 @@ from watchdog.observers.polling import PollingObserver
 
 from config.app import TARGET_DIR, TARGET_FILE, WEBHOOK_URL
 
+# prevの内容をファイルレベルで保持するための変更
+prev_logs = []
+
 
 def SendMessage(message: str) -> None:
-    # discord以外のサービスのwebhookの場合はここを変更してください
     main_content = {"content": message}
     requests.post(WEBHOOK_URL, main_content)
 
 
 def MessageCreation(text: str):
-    # 参加,退出以外のメッセージが必要な場合はここに書く
-    mach = re.findall(": (.*) joined the game", text)
-    if len(mach) == 1:
-        return str(mach[0]) + " が参加しました"
+    match = re.findall(": (.*) joined the game", text)
+    if len(match) == 1:
+        return str(match[0]) + " が参加しました"
 
-    mach = re.findall(": (.*) left the game", text)
-    if len(mach) == 1:
-        return str(mach[0]) + " が退出しました"
+    match = re.findall(": (.*) left the game", text)
+    if len(match) == 1:
+        return str(match[0]) + " が退出しました"
 
     return None
 
 
 def GetLog(filepath: str):
-    # 最終行のテキストを取得
-    with open(filepath, "r") as f:
-        endtxt = f.readlines()[-1]
+    global prev_logs  # グローバル変数を使用する代わりにファイルレベルの変数を更新
 
-    # 送信メッセージ作成
-    text = MessageCreation(endtxt)
+    with open(filepath, "r", errors="ignore") as f:
+        logs = f.readlines()
 
-    if text is not None:
-        SendMessage(text)
+    old_logs = copy.deepcopy(prev_logs)
+    prev_logs = copy.deepcopy(logs)
+
+    del logs[: len(old_logs)]
+
+    for log in logs:
+        text = MessageCreation(log)
+        if text is not None:
+            SendMessage(text)
 
 
 class ChangeHandler(FileSystemEventHandler):
-    # フォルダ変更時のイベント
     def on_modified(self, event):
         filepath = event.src_path
-
-        # ファイルでない場合無視する
-        if os.path.isfile(filepath) is False:
-            return
-
-        # 監視対応のファイルでない場合無視する
-        filename = os.path.basename(filepath)
-        if filename != TARGET_FILE:
-            return
-
-        GetLog(filepath)
+        if os.path.isfile(filepath) and os.path.basename(filepath) == TARGET_FILE:
+            GetLog(filepath)
 
 
-if __name__ in "__main__":
-    while 1:
-        event_handler = ChangeHandler()
-        observer = PollingObserver()
-        observer.schedule(event_handler, TARGET_DIR, recursive=True)
-        observer.start()
-        try:
-            while True:
-                time.sleep(0.1)
-        except KeyboardInterrupt:
-            observer.stop()
-        observer.join()
+if __name__ == "__main__":
+    with open(TARGET_DIR + "/" + TARGET_FILE, "r", errors="ignore") as f:
+        prev_logs = f.readlines()
+
+    event_handler = ChangeHandler()
+    observer = PollingObserver()
+    observer.schedule(event_handler, TARGET_DIR, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
