726d6bbd800f0c0792e185c9e65e9d49fa0763c3
somakai_sumasi <ueki@novasto.co.jp>
2025-03-28 15:50:25 +0900

    装飾削除処理追加

--- utils/log_message_parser.py
+++ utils/log_message_parser.py
@@ -11,5 +11,13 @@ def message_creation(text: str):
     for pattern, message_format in MESSAGE_PATTERNS:
         match = re.search(pattern, text)
         if match:
-            return message_format.format(*match.groups())
+            return remove_formatting(message_format.format(*match.groups()))
     return None
+
+
+def remove_formatting(text):
+    """
+    装飾に使われる「§」とその直後の、0～9またはa～u（大文字小文字）の文字を削除する関数
+    """
+    pattern = r'§[0-9a-uA-U]'
+    return re.sub(pattern, '', text)
